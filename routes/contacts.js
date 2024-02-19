a99f6f3e124097ea3588c6974d086e5e7cab5795
Rohan-IIT <rpawar3@hawk.iit.edu>
2024-02-18 22:26:42 -0600

    controller, routes added

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const contactsController = require('../controller/contactsController');
+const { body } = require('express-validator');
+
+/* GET contacts listing. */
+router.get('/', contactsController.contacts_list);
+
+/* GET contacts add */
+router.get('/add', contactsController.contacts_create_get);
+
+/* POST contacts add */
+router.post('/add', body('firstName').trim().notEmpty().withMessage('contact text can not be empty!'),
+    body('email').trim().not().isEmpty().withMessage('contact email address can not be empty!').isEmail().withMessage('Email must be a valid email address!'),
+     contactsController.contacts_create_post);
+
+/* GET a contact */
+router.get('/:uuid', contactsController.contacts_detail);
+
+/* GET contacts delete */
+router.get('/:uuid/delete', contactsController.contacts_delete_get);
+
+/* POST contacts delete */
+router.post('/:uuid/delete', contactsController.contacts_delete_post);
+
+/* GET contacts edit */
+router.get('/:uuid/edit', contactsController.contacts_edit_get);
+
+/* POST contacts add */
+router.post('/:uuid/edit', contactsController.contacts_edit_post);
+
+module.exports = router;
